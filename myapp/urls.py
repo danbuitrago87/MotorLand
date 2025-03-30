bb944c175a8e9d2f668d0ccbcd13a8abf88b2953
unknown <danbuitrago87@gmail.com>
2025-03-30 12:01:41 -0500

    Diseñando BD

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/',views.hello),
+    path('home/',views.index),
     path('home/about/', views.about),
+    path('hello/<str:username>',views.hello),
+    path('home/projects/', views.projects),
+    path('home/tasks/<str:title>', views.tasks),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("MotorLand")
+def hello(request,username):
+    print(username)
+    return HttpResponse("Hello %s" % username)
 
 def about(request):
     return HttpResponse("About")
+
+def index(request):
+    return HttpResponse("Index page")
+
+def projects(request):
+    projects=list (Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(request, title):
+    task=Task.objects.get(title=title)
+    return HttpResponse("task: %s" %task.title)
